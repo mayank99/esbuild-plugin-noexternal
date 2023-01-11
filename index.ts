5a368dd7c43cce5f6fd3d82ae65e925438ccc524
Mayank <mayank99@users.noreply.github.com>
2023-01-10 20:23:10 -0500

    support scoped packages

--- index.ts
+++ index.ts
@@ -1,16 +1,21 @@
 import type { Plugin } from 'esbuild';
 
 export default function externalizeAllPackagesExcept(noExternals: string[]) {
+	const getPackageName = (fullPath: string) => {
+		const splits = fullPath.split('/');
+		return fullPath.startsWith('@') ? splits.slice(0, 2).join('/') : splits[0];
+	};
+
 	return <Plugin>{
 		name: 'noExternal-plugin',
 		setup(build) {
 			if (noExternals.length > 0) {
 				build.onResolve({ filter: /(.*)/ }, (args) => {
-					if (
-						args.kind === 'import-statement' &&
-						!args.path.startsWith('.') &&
-						!noExternals.includes(args.path.split('/')[0])
-					) {
+					if (args.kind !== 'import-statement' || args.path.startsWith('.')) {
+						return;
+					}
+
+					if (!noExternals.includes(getPackageName(args.path))) {
 						return { path: args.path, external: true };
 					}
 				});
